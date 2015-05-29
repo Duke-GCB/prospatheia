555a50d8703df1f17355e58288998e71dd3fee9e
Dan Leehr <dan.leehr@duke.edu>
2015-05-29 16:27:33 -0400

    First functionality
    - and + to increase numbers, bound to pie chart
    not clear how to keep this at 100, unless i just show the total

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// Put code here
+angular.module('reportcard', [ 'n3-pie-chart' ])
+  .controller('ReportCardCtrl', function() {
+    var reportCard = this;
+    reportCard.title = 'GCB Effort Reporting';
+    reportCard.options = {thickness: 100, mode: "pie"};
+    reportCard.effort = [
+      {label: "R&D", value: 20, color: "red"},
+      {label: "Admin", value: 20, color: "green"},
+      {label: "Collaborative Research", value: 20, color: "blue"},
+      {label: "Infrastructure", value: 20, color: "orange"},
+      {label: "Tickets", value: 20, color: "purple"}
+    ];
+    reportCard.reduceEffort = function(effort) { effort.value = effort.value - 5;};
+    reportCard.increaseEffort = function(effort) { effort.value = effort.value + 5;};
+  });
