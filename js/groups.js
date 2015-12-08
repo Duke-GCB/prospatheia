6be910a36cd9d22bc213c505b867c0deda3f8c0c
Dan Leehr <dan.leehr@duke.edu>
2015-12-08 11:43:10 -0500

    Add remaining 2 users to sequencing core group

--- js/groups.js
+++ js/groups.js
@@ -118,7 +118,7 @@ var effortGroups = angular.module('ProspatheiaEffortGroupModule', []).service('E
         members: ['hhemric','novemberboys','rowle001','zzzwei'],
         categories : microarrayCategories },
       { name : 'sequencing',
-        members: [ 'gca2', 'nicholashoang', 'nicolasdevos1478', 'ofedrigo', 'wendyparris' ],
+        members: [ 'fy13duke', 'gca2', 'KaiaQ', 'nicholashoang', 'nicolasdevos1478', 'ofedrigo', 'wendyparris' ],
         categories : sequencingCategories },
     ];
 
